6c1519590421abada14a81c3b2859106b51d522a
ductablackgems <ducta@blackgemsglobal.com>
2025-01-20 20:41:36 +0700

    backup

---
+++ Assets/0.OW/Scripts/Minimap/OW_MiniMapController.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+namespace _0.OW.Scripts.Minimap
+{
+    public class OW_MiniMapController : MonoBehaviour
+    {
+        public Transform playerIcon;
+        
+     
+        public void Init()
+        {
+        }
+    }
+}
